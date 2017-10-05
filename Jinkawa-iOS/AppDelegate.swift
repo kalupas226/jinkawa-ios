56a8d42395b5c709fb1b779f67ddadfdbbfe6204
Taro Sato <taross@Tarossbook.local>
2017-10-05 17:32:06 +0900

    [add] プロジェクトファイルの追加

---
+++ Jinkawa-iOS/AppDelegate.swift
@@ -0,0 +1,50 @@
+//
+//  AppDelegate.swift
+//  Jinkawa-iOS
+//
+//  Created by Taro Sato on 2017/06/23.
+//  Copyright © 2017年 Taro Sato. All rights reserved.
+//
+
+import UIKit
+import NCMB
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    let applicationkey = ""
+    let clientkey = ""
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        NCMB.setApplicationKey("fe37c2186e22a438c980af699d831ac26d2ce6e05909c89e0677309528274a4d", clientKey: "9fd56b4ec717815b4d72081d9ae9e58192bdec9be30a416319d0069a6c33fd9f")
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ Jinkawa-iOS/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Jinkawa-iOS
+//
+//  Created by Taro Sato on 2017/06/23.
+//  Copyright © 2017年 Taro Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBAction func startButtonDidTapped(_ sender: Any) {
+        performSegue(withIdentifier: "showTabView", sender: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        let event = Event(eventName: "testName2", updateAt: "2:00", description: "testDescription")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func backToHome(segue: UIStoryboardSegue){
+        
+    }
+}
+
