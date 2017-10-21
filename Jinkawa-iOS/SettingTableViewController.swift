791fd363c762c74e796127014e5191195a2115f0
Kenta Aikawa <b1015110@fun.ac.jp>
2017-10-21 17:39:50 +0900

    設定画面にセクション適応

---
+++ Jinkawa-iOS/SettingTableViewController.swift
@@ -0,0 +1,106 @@
+//
+//  SettingTableViewController.swift
+//  Jinkawa-iOS
+//
+//  Created by Kenta Aikawa on 2017/10/21.
+//  Copyright © 2017年 Taro Sato. All rights reserved.
+//
+
+import UIKit
+
+let sectionTitle = ["設定", "お問合わせ"]
+let settingItem = ["通知のオン/オフ", "入力情報の確認"]
+let contactItem = ["陣川あさひ町会"]
+let tableData = [settingItem, contactItem]
+
+class SettingTableViewController: UITableViewController {
+
+    @IBOutlet weak var settingTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitle.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let sectionData = tableData[section]
+        return sectionData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitle[section]
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath)
+                switch indexPath.section {
+                case 0:
+                    cell.textLabel?.text = settingItem[indexPath.row]
+                case 1:
+                    cell.textLabel?.text = contactItem[indexPath.row]
+                default:
+                    break
+                }
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
