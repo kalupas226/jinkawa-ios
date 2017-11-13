047e95d93b13b0630ea0f547a2bad6696539d4e3
narihiro <narihiro@narihironoMacBook-Pro.local>
2017-11-13 20:52:47 +0900

    「ホームへ戻る」の実装
    設定タブをEurekaで実装し直しました.
    ついでにイベント詳細のオートレイアウトの修正を行いました.

--- Jinkawa-iOS/SettingViewController.swift
+++ Jinkawa-iOS/SettingViewController.swift
@@ -7,14 +7,13 @@
 //
 
 import UIKit
+import Eureka
 
 let sectionTitle = ["設定", "お問合わせ"]
 let settingItem = ["通知のオン/オフ", "入力情報の確認"]
 let contactItem = ["陣川あさひ町会"]
 
-class SettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    @IBOutlet weak var settingTableView: UITableView!
+class SettingViewController: FormViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +22,31 @@ class SettingViewController: UIViewController, UITableViewDelegate, UITableViewD
             navigationController?.navigationBar.largeTitleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         }
         
-        settingTableView.delegate = self
-        settingTableView.dataSource = self
-        
-        //空のセルの境界線を消す
-        settingTableView.tableFooterView = UIView(frame: .zero)
+        form
+            +++ Section("設定")
+            <<< SwitchRow() {
+                $0.title = "通知のオン/オフ"
+            }
+            <<< LabelRow() {
+                $0.title = "入力情報の確認"
+            }
+            +++ Section("アカウント")
+            <<< LabelRow() {
+                $0.title = "パスワード変更"
+            }
+            <<< LabelRow() {
+                $0.title = "ログアウト"
+            }
+            +++ Section("お問い合わせ")
+            <<< LabelRow() {
+                $0.title = "陣川あさひ町会"
+            }
+            +++ Section()
+            <<< LabelRow() {
+                $0.title = "ホーム画面へ戻る"
+                }.onCellSelection {cell, row in
+                    self.dismiss(animated: true, completion: nil)
+        }
         
     }
     
@@ -36,86 +55,6 @@ class SettingViewController: UIViewController, UITableViewDelegate, UITableViewD
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: - Table view data source
-    
-    //セクションの個数設定
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return sectionTitle.count
-    }
-    
-    //セクションの値設定
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sectionTitle[section]
-    }
-    
-    //セルの個数設定
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return settingItem.count
-        case 1:
-            return contactItem.count
-        default:
-            return 0
-        }
-    }
-    
-    //セルの値設定
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath)
-        
-        switch indexPath.section {
-        case 0:
-            cell.textLabel?.text = settingItem[indexPath.row]
-        case 1:
-            cell.textLabel?.text = contactItem[indexPath.row]
-        default:
-            break
-        }
-        
-        return cell
-    }
-    
-    //セルが選択された時に呼ばれるデリゲートメソッド
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("セクション番号：\(indexPath.section) セル番号：\(indexPath.row)")
-    }
-    
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
     
     /*
      // MARK: - Navigation
@@ -126,6 +65,7 @@ class SettingViewController: UIViewController, UITableViewDelegate, UITableViewD
      // Pass the selected object to the new view controller.
      }
      */
-
-
+    
+    
 }
+
